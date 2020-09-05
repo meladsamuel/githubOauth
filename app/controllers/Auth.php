324b4c91c8d9d21f843bd42da9839064abcde18d
meladsamuel <meladsamuel2@gmail.com>
2020-09-05 10:55:05 +0200

    first upload on heroku

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -71,6 +71,7 @@ class Auth extends AbstractController
             'code' => $_GET['code']
         ];
         $response = API::sendRequest($this->tokenURL, $this->session, $params);
+        echo $response->access_token;
         $this->session->access_token = $response->access_token;
         echo $this->session->access_token;
 //        $this->redirect('auth/callback');
